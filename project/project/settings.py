e50ed1926f5fb9f2a65b28580738fa481a157382
xym <38181202+2909619384@users.noreply.github.com>
2018-07-07 13:29:39 +0800

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-<<<<<<< HEAD
+
     'detail'
-=======
-    'cwdapp'
->>>>>>> cwd
+
+    'cwdapp',
+    
+    'xym'
+
 ]
 
 MIDDLEWARE = [
