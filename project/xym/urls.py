2999a8904d1e45ae8feb967632cf78ce1bd0f713
meimei <2909619384@qq.com>
2018-07-09 14:29:24 +0800

    add new house

--- project/xym/urls.py
+++ project/xym/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     #上传新房源
     url(r'^addnewhouse/',views.my_new_house,name='my_new_house'),
     #房源添加后展示
-    url(r'^shownewhouse/',views.show_newhouse,name='show_new_house'),
+    # url(r'^shownewhouse/',views.show_newhouse,name='show_new_house'),
     #成功添加房源后跳转页面
     url(r'^success/', views.show_success, name='show_success'),
-    url(r'^removenewhouse/',views.remove_newhouse,name='remove_newhouse')
+    # url(r'^removenewhouse/',views.remove_newhouse,name='remove_newhouse')
 
 
 ]
--- project/xym/views.py
+++ project/xym/views.py
@@ -9,13 +9,6 @@ from django.core.files.storage import FileSystemStorage
 from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
-
-# def hello(request):
-#     if request.method == 'GET':
-#         return render(request, 'test.html')
-# 首页
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
@@ -24,34 +17,23 @@ from app.models import Facility, Area, HouseType, House, User, HouseDetail, Hous
 
 # 用户登录
 def login(request):
-    # get请求获取登录页面
     if request.method == 'GET':
         return render(request, 'xym/login.html')
 
     # post请求获取信息
     if request.method == 'POST':
-        username = request.POST.get('account')  # because is name='account' in html
+        username = request.POST.get('account')
         password = request.POST.get('password')
-        #just print to check the account and password you get
-        print(username)
-        print(password)
 
-        # 判断账号是否存在
-        #search from the mysql if the username is exist
-        if User.objects.filter(account=username).exists():  # chec kif the user exist
-            users = User.objects.filter(account=username)  # 获取的是列表类型 # get row from database, index 0 because 1 result
-            print('users:')
-            print(users)
-            # 检查密码
-            # if check_password(password, users[0].password): # check the hashed password of the user
+        if User.objects.filter(account=username).exists():
+            users = User.objects.filter(account=username)
+
+            # if check_password(password, users[0].password):
             if password == users[0].password:
 
                 # 将登录的账户名传递给session对象
+                request.session['account'] = username
 
-                request.session['account'] = username  # store user into session after login
-                # request.session['yanmei_data']=123
-                # request.session['user_id'] = users[0].user_id
-                # session is server side database
                 return HttpResponseRedirect('/xym/index.html')
             else:  # wrong password
                 return HttpResponse('登录密码错误')
@@ -62,70 +44,50 @@ def login(request):
 # 上传新房源
 @csrf_exempt
 def my_new_house(request):
-    # yanmei=request.session.get('yanmei_data') # yanmei will be equal to 123 now
+    username = request.session.get('account')
+    users = User.objects.filter(account=username)
+    user_id = users[0].user_id
 
-    # OPTION A
-    # this is different website, we want to get session
-    # find who is login now
-    username = request.session.get('account')  # get username from session, we need to login first !!!
-    # after session we check database for user_id for username
-    users = User.objects.filter(account=username)  # search database,  get users from database with username user
-    user_id = users[0].user_id  # we get user_id from first one (row 0, always 0)
-    print('user_id:')
-    print(user_id)
-
-    # OPTION B
-
-    # another way to get user id (faster, no database)
-    # user_id = request.session.get('user_id') # after login
-
-    if request.method == 'GET': # here we send data to html
-        #get all the area from mysql
+    if request.method == 'GET':
         area = Area.objects.all()
         facility_name = Facility.objects.all()
         facilitys_id = Facility.objects.all
         type_names = HouseType.objects.all()
-        data = { # the data html use to display: area, facility
+        data = {
             'area': area,
             'facility_name': facility_name,
             'type_names': type_names,
             'facility_id': facilitys_id
         }
         if user_id:
-            #if the user is login
+            # if the user is login
             return render(request, 'xym/newhouse.html', data)
         else:
-            #if not login return to login page
+            # if not login return to login page
             return render(request, 'xym/login.html')
 
-    if request.method == 'POST': # here we get the data from form html (name=)
-        # data = {
-        #     'msg': '请求成功',
-        #     'code': 200
-        # }
+    if request.method == 'POST':
 
         title = request.POST.get('title')
         # 价格
         price = request.POST.get('price')
-        print('price')
-        print(price)
+
         # 支付方式
         pay_way = request.POST.get('pay_way')
-        print('支付')
-        print(pay_way)
+
         # 租赁方式
         lease = request.POST.get('lease')
         # 区域
         area_id = request.POST.get('area_id')  # this is the name as in html under name=
-        print('area')
-        print(area_id)
+
         # 地址
         address = request.POST.get('address')
         # 面积
         acreage = request.POST.get('acreage')
         # 房型
         type = request.POST.get('type')
-        type_id = 1  # type.type_id
+        type_id = 1
+
         # 楼层
         floor = request.POST.get('floor')
         # 朝向
@@ -140,26 +102,15 @@ def my_new_house(request):
         # house_form = House(data=request.POST)
         # 房屋图片
         img_file = request.FILES.get('house_image')
-        #use the FileSystemStorage() function
+        # use the FileSystemStorage() function
         fs = FileSystemStorage()
         filename = fs.save(img_file.name, img_file)
-        index_img_url=fs.url(filename)
-        print('image')
-        print(index_img_url)
-
+        index_img_url = fs.url(filename)
 
         facilitys_id = request.POST.getlist('facilitys')  # get name from html
-        print('facility_id')
-        print(facilitys_id)
 
-         # search database for name to get object
-         # get id from object that we find
-
-
-        #房屋状态
+        # 房屋状态
         house_status = request.POST.get('house_statuss')
-
-
 
         # insert into the databases
         house = House.objects.create(
@@ -175,12 +126,8 @@ def my_new_house(request):
             house_status=house_status
 
         )
-        house_id = house.house_id  # we get the house id from the new object
-
-        # my_house = request.POST.get('house_id')
-        # house_id = House.objects.filter(house_id=my_house).first()
+        house_id = house.house_id
         house_detail = HouseDetail.objects.create(
-            # another table ,is not house table, the house detail need house id  to connect
             house_id=house_id,
             lease=lease,
             pay_way=pay_way,
@@ -198,59 +145,19 @@ def my_new_house(request):
 
         )
 
-        #for loop over facility_id to add lines to facility database
+        # for loop over facility_id to add lines to facility database
         for facility_id in facilitys_id:
             facility = HouseFacility.objects.create(
 
                 house_id=house_id,
-                ## database column name = your name
                 facility_id=facility_id
             )
 
-        #return JsonResponse(data)
-        return render(request,'xym/success.html')
+        return render(request, 'xym/success.html')
 
-
-
-#添加新房源后展示
-def show_newhouse(request):
-    # get user from session. after he login
-    # get house  from database for the user
-    #send user  and house  to my—house.html—
-    users = request.session.get('account')
-    user = User.objects.filter(account=users)
-    user_id = user[0].user_id
-
-    #find the house information vy user_id
-    house = House.objects.filter(user_id=user_id)
-    data = {
-        'user':user,
-        'house':house
-    }
-    if user:
-        if request.method == 'GET':
-            return render(request, 'xym/showHouse.html', data)
-    else:
-        return render(request,'xym/login.html')
 
 
 
 def show_success(request):
-
     if request.method == 'GET':
-        return render(request,'xym/success.html')
-
-#删除新增的房源
-@csrf_exempt
-def remove_newhouse(request):
-    if request.method == 'GET':
-        return render(request,'xym/showHouse.html')
-
-    if request.method == 'POST':
-        houses = request.POST.get('house_id')
-        print(houses)
-        HouseDetail.objects.filter(house_id=houses).delete() # remove the house from table, where house_id is foreign key
-        HouseFacility.objects.filter(house_id=houses).delete() # remove the house from table, where house_id is foreign key
-        HouseImg.objects.filter(house_id=houses).delete() # remove the house from table, where house_id is foreign key
-        House.objects.filter(house_id=houses).delete() # at last we remove the house from main table, where house_id is MAIN key
-        return render(request,'xym/showHouse.html')
+        return render(request, 'xym/success.html')
